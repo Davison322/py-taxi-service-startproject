27e1fde248eea86fd6c2e1e4504f88d93bf15a6d
Dmytro Kolodii <dimakolodiy.ua@gmail.com>
2025-01-22 17:19:57 +0200

    sol2

--- taxi/admin.py
+++ taxi/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from taxi.models import *
+from taxi.models import Manufacturer, Car, Driver
 # Register your models here.
 
 admin.site.register(Manufacturer)
--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from key import Key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=+!b!2!xcrq6z#6cbf*f@$d*)fn=-7tvqspr_6rntjel!%bz@$'
+SECRET_KEY = Key
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'localhost',
+]
 
 
 # Application definition
