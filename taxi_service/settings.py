8e1f35b4859e4be6cd66deab0180cb6a097ff8c0
Dmytro Kolodii <dimakolodiy.ua@gmail.com>
2025-01-22 17:23:20 +0200

    sol3

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-from key import Key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = Key
+SECRET_KEY = 'django-insecure-=+!b!2!xcrq6z#6cbf*f@$d*)fn=-7tvqspr_6rntjel!%bz@$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
